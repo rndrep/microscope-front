fa32ec9a9b9d8b3e245ebf162e9f8923f7544f05
vvk61 <vvk61@tpu.ru>
2021-08-19 17:59:08 +0700

    fixed gallery

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,6 +1,12 @@
 //= ../../../node_modules/slick-carousel/slick/slick.js
 //= components/map.js
 
-$(function () {
+$(function () {});
+
+$("#accordion-button-gallery").on("click", function (event) {
     //= components/slick.js
+});
+
+$("#accordion-button-map").on("click", function (event) {
+    //= components/map.js
 });
--- src/assets/js/components/map.js
+++ src/assets/js/components/map.js
@@ -1,24 +1,30 @@
-// var mymap = L.map("map").setView([56.471, 84.957], 13);
+var container = L.DomUtil.get("map");
+if (container != null) {
+    container._leaflet_id = null;
+}
 
-// L.tileLayer(
-//     "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
-//     {
-//         attribution:
-//             'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-//         maxZoom: 18,
-//         id: "mapbox/streets-v11",
-//         tileSize: 512,
-//         zoomOffset: -1,
-//         accessToken:
-//             "pk.eyJ1IjoidnZrNjEiLCJhIjoiY2tzNXgyMTZrMDViaTJ1cHNxbDhsbXhzcyJ9.me3r1SBREWSOPn_A3Nx5yQ",
-//     }
-// ).addTo(mymap);
+var mymap = L.map("map").setView([56.471, 84.957], 13);
 
-// initialize_map();
+L.tileLayer(
+    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+    {
+        attribution:
+            'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: "mapbox/streets-v11",
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken:
+            "pk.eyJ1IjoidnZrNjEiLCJhIjoiY2tzNXgyMTZrMDViaTJ1cHNxbDhsbXhzcyJ9.me3r1SBREWSOPn_A3Nx5yQ",
+    }
+).addTo(mymap);
+
+// mymap.invalidateSize();
 
 // $("#map").on("hidden.bs.collapse", function () {
 //     initialize_map();
 // });
+
 // $("#map").on("shown.bs.collapse", function () {
 //     initialize_map();
 // });
