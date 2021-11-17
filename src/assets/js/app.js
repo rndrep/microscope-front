ab22a9811bff0eca35f05653221e2bb82af1fc1a
Your Name <hoshizone@gmail.com>
2021-11-18 02:15:21 +0700

    added select module

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -2,11 +2,11 @@
 
 import $ from "jquery";
 import "bootstrap";
-import SlimSelect from "slim-select";
 
 import map from "./modules/map";
 import microscope from "./modules/microscope";
 import slick from "./modules/slick";
+import select from "./modules/select";
 
 window.addEventListener("DOMContentLoaded", () => {
 	// slick-carousel
@@ -19,57 +19,8 @@ window.addEventListener("DOMContentLoaded", () => {
 		map();
 	});
 
-	let all = document.querySelectorAll(".single");
-	// console.log(all);
-
-	// all.forEach((element, index, array) => {
-	// 	element = new SlimSelect({
-	// 		select: element,
-	// 		searchPlaceholder: "Поиск",
-	// 		deselectLabel: "Не найдено",
-	// 		showSearch: "false",
-	// 		searchText: "Не найдено",
-	// 	});
-	// });
-
-	document.querySelectorAll(".select-single").forEach((element) => {
-		new SlimSelect({
-			select: element,
-			searchPlaceholder: "Поиск",
-			showSearch: "false",
-			searchText: "Не найдено",
-			placeholder: "-",
-			deselectLabel: "×",
-			allowDeselect: true,
-			showContent: "down",
-			onChange: (element) => {
-				console.log(element);
-			},
-		});
-	});
-
-	document.querySelectorAll(".select-multiply").forEach((element) => {
-		var select = new SlimSelect({
-			select: ".select-multiply",
-			showContent: "auto",
-			limit: false,
-			searchPlaceholder: "Поиск",
-			showSearch: "false",
-			searchText: "Не найдено",
-			placeholder: "-",
-			deselectLabel: "×",
-			allowDeselect: true,
-			hideSelectedOption: true,
-			closeOnSelect: false,
-			showContent: "down",
-			onChange: (element) => {
-				console.log(element);
-			},
-		});
-	});
-	console.log(select.selected());
-
-	// microscope();
+	select();
+	microscope("http://vmicro.tpu.ru/microscope/rock/test");
 });
 
 export default microscope;
